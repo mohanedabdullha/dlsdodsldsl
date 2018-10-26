3393d8ff34a4bc6e77c68294af3b445c6035ac1a
mohanedabdullha <43791989+mohanedabdullha@users.noreply.github.com>
2018-10-27 00:56:59 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`Magic Server. New`,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`Magic Server`,'https://www.twitch.tv/MeeRcY')
 
 });
 client.login(process.env.BOT_TOKEN);
